3586ff93b70d27e48999e60bb7bd008eb0038565
Naveena-04 <naveenasharma895@gmail.com>
2024-08-30 20:05:57 +0530

    Update passwordgen.py

--- passwordgen.py
+++ passwordgen.py
@@ -8,7 +8,7 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# Easy Level
+
 # password = ""
 # for char in range(0, nr_letters):
 #     password += random.choice(letters)
@@ -21,7 +21,6 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #
 # print(password)
 
-# Hard level
 password_list = []
 for char in range(0, nr_letters):
     password_list.append(random.choice(letters))
