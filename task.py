0c67de73b4f1f02f3f614deb5f449045de090143
Naveena-04 <naveenasharma895@gmail.com>
2024-08-30 19:56:05 +0530

    Update task.py

--- task.py
+++ task.py
@@ -35,11 +35,11 @@ if choice>=3 and choice<0:
     print("you typed invalid number so you lose :(")
 elif choice==0 and ch==2: # rock and scissors
     print("you win !")
-elif ch==0 and choice==2:# rock of comp and man has scissors
+elif ch==0 and choice==2:
     print("you lose")
-elif choice>ch: # 2>1,2>0,1>0 is scissors >paper,rock  paper>rock
+elif choice>ch: 
     print("you win!")
-elif ch>choice: #2>1,2>0,1>0
+elif ch>choice: 
     print("you lose")
 elif ch==choice: #if both same
     print("it is draw")
